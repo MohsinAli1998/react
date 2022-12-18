c41ddb74e1b864d353892a471803f2ce36e5c729
MohsinAli1998 <shmohsin102@gmail.com>
2022-12-18 01:03:19 -0800

    new feed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Mart from './mart.js';
 function App() {
   return (
     <>
-    <div className="App">
+    {/* <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div> */}
     <div>
     <Mart />  
     </div></>
